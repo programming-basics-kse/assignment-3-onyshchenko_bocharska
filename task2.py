eaa8b866b857c82771538d253da39d900d2ed616
kbocharska <kbocharska@kse.org.ua>
2024-11-25 23:08:56 +0200

    add task3
    added task3

--- task2.py
+++ task2.py
@@ -1,4 +1,5 @@
 import argparse
+from pyexpat.errors import messages
 
 parser = argparse.ArgumentParser("Analyze Olympic medal data.")
 
@@ -17,9 +18,32 @@ parser.add_argument("-total",
                     nargs = 1,
                     metavar = "year")
 
+parser.add_argument("-overall",
+                    nargs = "+",
+                    metavar = "country",
+                    help = "Pick at least one country")
+
 args = parser.parse_args()
 
-# print(args)
+def get_data(file_name):
+    countries_dict = {}
+    with open(file_name, "r") as datafile:
+        next(datafile)
+        for line in datafile:
+            athlete = make_athlete(line)
+            if athlete["medal"] == "NA":
+                continue
+            if athlete["country"] not in countries_dict:
+                countries_dict[athlete["country"]] = {}
+            if athlete["year"] not in countries_dict[athlete["country"]]:
+                countries_dict[athlete["country"]][athlete["year"]] = {}
+            if athlete["medal"] not in countries_dict[athlete["country"]][athlete["year"]]:
+                countries_dict[athlete["country"]][athlete["year"]][athlete["medal"]] = 1
+            if "total" not in countries_dict[athlete["country"]][athlete["year"]]:
+                countries_dict[athlete["country"]][athlete["year"]]["total"] = 1
+            countries_dict[athlete["country"]][athlete["year"]][athlete["medal"]] += 1
+            countries_dict[athlete["country"]][athlete["year"]]["total"] += 1
+        return countries_dict
 
 def make_athlete(line):
     [id, name, sex, age, height, weight, team, noc, games, year, season, city, sport, event, medal] = line.split('\t')
@@ -49,7 +73,7 @@ if args.medals:         # тут я додав умову щоб код вико
     with open(args.data_file) as file:
         medals = {"gold": 0, "silver": 0, "bronze": 0}
 
-        file.readline()
+        next(file)
         athletes = [make_athlete(line) for line in file if is_good(line)]
 
         for idx, athlete in enumerate(athletes):
@@ -82,5 +106,33 @@ if args.total:
         if args.output:
             with open(args.output, "a") as file:
                 file.write(text + "\n")
+
+
+def find_max(country_list, country_name):
+    years = country_list[country_name].keys()
+    max = 0
+    best_year = ""
+    for year in years:
+        max_check = country_list[country_name][year]["total"]
+        if max_check > max:
+            max = max_check
+            best_year = year
+    return max, best_year
+
+def print_result(result_dict, country_name):
+    message = f"{country_name}'s best result was in {result_dict[country_name][1]} with a total of {result_dict[country_name][0]} medals"
+    return message
+
+if args.overall:
+    countries = get_data(args.data_file)
+    totals = {}
+    for country in args.overall:
+        total = find_max(countries, country)
+        totals[country] = total
+        text = print_result(totals, country)
+    if args.output:
+        with open(args.output, "a") as file:
+            file.write(text + "\n")
+
 
 
